8daf48b8ea281750186570d2efa0f0f07f3a5e11
Turgay Mutlu <mutlu.turgay@gmail.com>
2019-02-23 17:59:07 +0300

    MainActivity refactor

--- app/src/main/java/com/mutlu/turgay/kotlincleanarchitecturesample/MainActivity.kt
+++ app/src/main/java/com/mutlu/turgay/kotlincleanarchitecturesample/MainActivity.kt
@@ -2,11 +2,9 @@ package com.mutlu.turgay.kotlincleanarchitecturesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mutlu.turgay.kotlincleanarchitecturesample.base.BaseActivity
+import com.mutlu.turgay.kotlincleanarchitecturesample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val layoutRes: Int = R.layout.activity_main
 }
