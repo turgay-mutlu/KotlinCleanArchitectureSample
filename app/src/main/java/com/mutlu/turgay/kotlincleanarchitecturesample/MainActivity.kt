ba73e8c8c80a7facfe953302c9d18d9c1935ee58
Turgay Mutlu <mutlu.turgay@gmail.com>
2019-02-23 14:47:43 +0300

    Migrate to Androidx

--- app/src/main/java/com/mutlu/turgay/kotlincleanarchitecturesample/MainActivity.kt
+++ app/src/main/java/com/mutlu/turgay/kotlincleanarchitecturesample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.mutlu.turgay.kotlincleanarchitecturesample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
